981401e939408abf1dedd1a0e04c7bb54ab883fe
kevin <kevin.dunphy1989@gmail.com>
2020-12-03 09:05:08 -0400

    Day 3 easier to read

--- AOC2020/solutions/day3.py
+++ AOC2020/solutions/day3.py
@@ -3,15 +3,12 @@ slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
 
 
 def tree_slope(right, down):
-    with open('../inputs/day3.txt') as f:
-        lines = f.readlines()
-
+    lines = [line.strip() for line in open('../inputs/day3.txt')]
     tree_count, idx = 0, 0
     for line in lines[down::down]:
         idx += right
-        idx -= len(line.strip()) if idx >= len(line.strip()) else 0
+        idx -= len(line) if idx >= len(line) else 0
         tree_count += 1 if line[idx] == '#' else 0
-
     return tree_count
 
 
