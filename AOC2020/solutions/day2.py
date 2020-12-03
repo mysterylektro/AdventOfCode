bd56948f7a6d8526628c570b6e72cc625c561ea0
kevin <kevin.dunphy1989@gmail.com>
2020-12-03 07:46:22 -0400

    I like this day 2 solution better

--- AOC2020/solutions/day2.py
+++ AOC2020/solutions/day2.py
@@ -1,7 +1,18 @@
+import re
+
+
+def valid_one(lower, upper, letter, password):
+    return int(lower) <= password.count(letter) <= int(upper)
+
+
+def valid_two(p1, p2, letter, password):
+    return (password[int(p1) - 1] == letter) != (password[int(p2) - 1] == letter)
+
+
 count1, count2 = 0, 0
 with open('../inputs/day2.txt') as f:
     for line in f:
-        args = *tuple(map(int, line.split()[0].split('-'))), line.split()[1][0], line.split()[2]
-        count1 += 1 if args[0] <= args[3].count(args[2]) <= args[1] else 0
-        count2 += 1 if (args[3][args[0]-1] == args[2]) != (args[3][args[1]-1] == args[2]) else 0
+        args = re.split('-|: | ', line.strip())
+        count1 += 1 if valid_one(*args) else 0
+        count2 += 1 if valid_two(*args) else 0
 print(f"Part 1 Answer: {count1}\nPart 2 Answer: {count2}")
