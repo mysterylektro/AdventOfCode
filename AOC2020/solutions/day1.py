79fb0b2451070eaed8e73dbaf3ab24ea2f110f61
kevin <kevin.dunphy1989@gmail.com>
2020-12-03 10:03:07 -0400

    Day 1 Condensed

--- AOC2020/solutions/day1.py
+++ AOC2020/solutions/day1.py
@@ -1,10 +1,5 @@
 import itertools
 import numpy as np
 
-with open('../inputs/day1.txt') as f:
-    nums = list(map(int, f.readlines()))
-
-answer = np.prod([combo for combo in itertools.combinations(nums, 2) if sum(combo) == 2020][0])
-print(f"Part 1 Answer: {answer}")
-answer = np.prod([combo for combo in itertools.combinations(nums, 3) if sum(combo) == 2020][0])
-print(f"Part 2 Answer: {answer}")
+nums = [int(line.strip()) for line in open('../inputs/day1.txt')]
+print(f"Part 1 Answer: {np.prod([combo for combo in itertools.combinations(nums, 2) if sum(combo) == 2020][0])}\nPart 2 Answer: {np.prod([combo for combo in itertools.combinations(nums, 3) if sum(combo) == 2020][0])}")
