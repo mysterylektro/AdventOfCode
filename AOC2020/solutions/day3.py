0185a37b536ea95116de50edff519105c0102ccd
kevin <kevin.dunphy1989@gmail.com>
2020-12-03 09:08:46 -0400

    Day 3 condensed.

--- AOC2020/solutions/day3.py
+++ AOC2020/solutions/day3.py
@@ -3,14 +3,11 @@ slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
 
 
 def tree_slope(right, down):
-    lines = [line.strip() for line in open('../inputs/day3.txt')]
-    tree_count, idx = 0, 0
+    lines, tree_count, idx = [line.strip() for line in open('../inputs/day3.txt')], 0, 0
     for line in lines[down::down]:
-        idx += right
-        idx -= len(line) if idx >= len(line) else 0
+        idx += right if idx < len(line)-right else right-len(line)
         tree_count += 1 if line[idx] == '#' else 0
     return tree_count
 
 
-print(f"Part 1 Answer: {tree_slope(3, 1)}")
-print(f"Part 2 Answer: {np.prod([tree_slope(*args) for args in slopes])}")
+print(f"Part 1 Answer: {tree_slope(*slopes[1])}\nPart 2 Answer: {np.prod([tree_slope(*args) for args in slopes])}")
