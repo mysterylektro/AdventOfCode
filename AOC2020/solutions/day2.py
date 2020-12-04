4c4b5f3a7c44d2dc372d7f13b580fbbdb3b55c49
kevin <kevin.dunphy1989@gmail.com>
2020-12-04 13:17:52 -0400

    Day 2 Further condensed

--- AOC2020/solutions/day2.py
+++ AOC2020/solutions/day2.py
@@ -1,9 +1,7 @@
 import re
 
 
-lines, count1, count2 = [line.strip() for line in open('../inputs/day2.txt')], 0, 0
-for line in lines:
-    args = re.split('-|: | ', line)
-    count1 += 1 if int(args[0]) <= args[3].count(args[2]) <= int(args[1]) else 0
-    count2 += 1 if (args[3][int(args[0]) - 1] == args[2]) != (args[3][int(args[1]) - 1] == args[2]) else 0
+arg_list, count1, count2 = [re.split('-|: | ', line.strip()) for line in open('../inputs/day2.txt')], 0, 0
+for args in arg_list:
+    count1, count2 = count1 + (1 if int(args[0]) <= args[3].count(args[2]) <= int(args[1]) else 0), count2 + (1 if (args[3][int(args[0]) - 1] == args[2]) != (args[3][int(args[1]) - 1] == args[2]) else 0)
 print(f"Part 1 Answer: {count1}\nPart 2 Answer: {count2}")
