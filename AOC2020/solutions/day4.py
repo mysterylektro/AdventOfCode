a8427d91c2a8dd5fe9edcf34ac8048c274a38f29
kevin <kevin.dunphy1989@gmail.com>
2020-12-04 09:14:11 -0400

    Day 4 Condensed

--- AOC2020/solutions/day4.py
+++ AOC2020/solutions/day4.py
@@ -1,52 +1,23 @@
 import string
 
 
-def valid_hgt(hgt):
-    if hgt[-2:] == 'cm':
-        return 150 <= int(hgt[:-2]) <= 193
-    elif hgt[-2:] == 'in':
-        return 59 <= int(hgt[:-2]) <= 76
-    else:
-        return False
-
-
 options = {'byr': lambda byr: 1920 <= int(byr) <= 2002,
            'iyr': lambda iyr: 2010 <= int(iyr) <= 2020,
            'eyr': lambda eyr: 2020 <= int(eyr) <= 2030,
-           'hgt': valid_hgt,
+           'hgt': lambda hgt: 150 <= int(hgt[:-2]) <= 193 if 'cm' in hgt else 59 <= int(hgt[:-2]) <= 76 if 'in' in hgt else False,
            'hcl': lambda hcl: hcl[0] == '#' and len(hcl[1:]) == 6 and all([h in string.hexdigits for h in hcl[1:]]),
            'ecl': lambda ecl: ecl in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'],
            'pid': lambda pid: len(pid) == 9 and all([i in string.digits for i in pid])}
 
-
-def valid_pp_part1(passport):
-    return all([k in passport for k in options])
-
-
-def valid_pp_part2(passport):
-    return all([func(passport[k]) for k, func in options.items() if k in passport])
-
-
-part1, part2 = 0, 0
-lines = [line.strip() for line in open('../inputs/day4.txt')]
-
-pp = {}
+part1, part2, pp, lines = 0, 0, {}, [line.strip() for line in open('../inputs/day4.txt')] + ['']
 for line in lines:
     if line == '':
-        p1, p2 = valid_pp_part1(pp), valid_pp_part2(pp)
+        p1, p2, pp = all([k in pp for k in options]), all([func(pp[k]) for k, func in options.items() if k in pp]), {}
         part1 += 1 if p1 else 0
         part2 += 1 if p1 and p2 else 0
-        pp = {}
         continue
-    tokens = line.split()
-    for token in tokens:
+    for token in line.split():
         key, value = token.split(':')
         pp[key] = value
 
-if pp != {}:
-    p1, p2 = valid_pp_part1(pp), valid_pp_part2(pp)
-    part1 += 1 if p1 else 0
-    part2 += 1 if p1 and p2 else 0
-
-print(f"Part 1 Answer: {part1}")
-print(f"Part 2 Answer: {part2}")
+print(f"Part 1 Answer: {part1}\nPart 2 Answer: {part2}")
