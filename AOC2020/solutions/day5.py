6e51238e4394cf97f959537832b40d99b3635e7e
kevin <kevin.dunphy1989@gmail.com>
2020-12-05 08:31:03 -0400

    Day 5 further condensed.

--- AOC2020/solutions/day5.py
+++ AOC2020/solutions/day5.py
@@ -1,4 +1,3 @@
-lines = [line.strip() for line in open('../inputs/day5.txt')]
-seat_ids = [int(line[:7].replace('F', '0').replace('B', '1'), 2) * 8 + int(line[7:].replace('L', '0').replace('R', '1'), 2) for line in lines]
+seat_ids = [int(line.translate(line.maketrans('FBLR', '0101')).strip(), 2) for line in open('../inputs/day5.txt')]
 my_seat = [seat for seat in list(range(min(seat_ids), max(seat_ids))) if seat not in seat_ids][0]
 print(f"Part 1 Answer: {max(seat_ids)}\nPart 2 Answer: {my_seat}")
