866ce9d0cc6175989b3023e849a4128badd150f8
kevin <kevin.dunphy1989@gmail.com>
2020-12-06 18:06:22 -0400

    Day 6 Condensed

--- AOC2020/solutions/day6.py
+++ AOC2020/solutions/day6.py
@@ -1,4 +1,4 @@
 groups, group = [], []
 for line in [line.strip() for line in open('../inputs/day6.txt')] + ['']:
-    groups, group = (groups + [group], []) if line == '' else (groups, group + [set(list(line))])
+    groups, group = (groups + [group], []) if line == '' else (groups, group + [{*line}])
 print(f"Part 1 Answer: {sum([len(set.union(*g)) for g in groups])}\nPart 2 Answer: {sum([len(set.intersection(*g)) for g in groups])}")
