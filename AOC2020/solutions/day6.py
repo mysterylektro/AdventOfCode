2ac869dcc42e0752f654b1637960981b83864fc8
kevin <kevin.dunphy1989@gmail.com>
2020-12-06 17:55:02 -0400

    Day 6 Condensed

--- AOC2020/solutions/day6.py
+++ AOC2020/solutions/day6.py
@@ -1,21 +1,4 @@
-def get_count(lines, part1=True):
-    groups, group = [], None
-    for line in lines:
-        if line == '\n':
-            groups.append(group)
-            group = None
-            continue
-        if group is None:
-            group = set([char for char in line.strip()])
-            continue
-        if part1:
-            group = group.union(set([char for char in line.strip()]))
-        else:
-            group = group.intersection(set([char for char in line.strip()]))
-
-    return sum([len(g) for g in groups])
-
-
-lines = [line for line in open('../inputs/day6.txt')] + ['\n']
-print(f"Part 1 Answer: {get_count(lines)}")
-print(f"Part 2 Answer: {get_count(lines, part1=False)}")
+groups, group = [], []
+for line in [line.strip() for line in open('../inputs/day6.txt')] + ['']:
+    groups, group = (groups + [group], []) if line == '' else (groups, group + [set(list(line))])
+print(f"Part 1 Answer: {sum([len(set.union(*g)) for g in groups])}\nPart 2 Answer: {sum([len(set.intersection(*g)) for g in groups])}")
