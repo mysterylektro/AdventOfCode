23351580a53f75242123082b1072af97bb75104b
kevin <kevin.dunphy1989@gmail.com>
2020-12-07 08:51:03 -0400

    Day 7 condensed. Likely better options for parsing. Can maybe do recursive functions on one line instead of a function definition via lambda??

--- AOC2020/solutions/day7.py
+++ AOC2020/solutions/day7.py
@@ -3,40 +3,16 @@ bags = {}
 
 
 def can_contain_shiny_gold(bag, bag_dict):
-    contain_list = bag_dict[bag]
-    if len(contain_list) == 0:
-        return False
-    if 'shiny gold' in [b for b, c in contain_list]:
-        return True
-
-    return any([can_contain_shiny_gold(b, bag_dict) for b, c in contain_list])
+    return False if len(bag_dict[bag]) == 0 else True if 'shiny gold' in [b for b, c in bag_dict[bag]] else any([can_contain_shiny_gold(b, bag_dict) for b, c in bag_dict[bag]])
 
 
 def bag_sum(bag, bag_dict):
-    contain_list = bag_dict[bag]
-    if len(contain_list) == 0:
-        return 0
-    else:
-        return sum([int(c) + int(c)*bag_sum(b, bag_dict) for b, c in contain_list])
+    return 0 if len(bag_dict[bag]) == 0 else sum([c + c*bag_sum(b, bag_dict) for b, c in bag_dict[bag]])
 
 
 for line in open('../inputs/day7.txt'):
-    line = line.replace('.', ' ').strip()
-    tokens = line.split(' bags contain ')
-    bag_color = tokens[0]
-    contains = []
-    if 'no other bags' in tokens[1]:
-        bags[bag_color] = []
-        continue
-    for other_bags in re.split(' bag, | bags, | bag| bags', tokens[1])[:-1]:
-        count, colour = other_bags.split(' ')[0], ' '.join(other_bags.split(' ')[1:])
-        contains.append((colour, count))
-
+    bag_color, contains = line.replace('.', ' ').strip().split(' bags contain ')[0], [] if 'no' in line.replace('.', ' ').strip().split(' bags contain ')[1] else [(' '.join(other_bags.split(' ')[1:]), int(other_bags.split(' ')[0])) for other_bags in re.split(' bag, | bags, | bag| bags', line.replace('.', ' ').strip().split(' bags contain ')[1])[:-1]]
     bags[bag_color] = contains
 
-count = 0
-for bag in bags:
-    count += 1 if can_contain_shiny_gold(bag, bags) else 0
-
-print(f"Part 1 Answer: {count}")
+print(f"Part 1 Answer: {sum([1 if can_contain_shiny_gold(bag, bags) else 0 for bag in bags])}")
 print(f"Part 2 Answer: {bag_sum('shiny gold', bags)}")
