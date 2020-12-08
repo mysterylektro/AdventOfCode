173729201caf285c9d2d3cbe13c88135601ceea0
kevin <kevin.dunphy1989@gmail.com>
2020-12-08 16:49:22 -0400

    Day 8 condensed

--- AOC2020/solutions/day8.py
+++ AOC2020/solutions/day8.py
@@ -1,56 +1,26 @@
-def nop(v, a, i):
-    i += 1
-    return a, i
+def bootcode(lines):
+    acc, idx, seen = 0, 0, set()
+    while True:
+        if idx in seen:
+            return False, acc
+        l = lines[idx].strip().split()
+        op, arg = l[0], int(l[1])
+        seen.add(idx)
+        acc += arg if op == 'acc' else 0
+        idx += arg if op == 'jmp' else 1
+        if idx >= len(lines):
+            return True, acc
 
-
-def jmp(v, a, i):
-    i += v
-    return a, i
-
-
-def acc(v, a, i):
-    a += v
-    i += 1
-    return a, i
-
-def replace_nop_jmp(line_list, i):
-    indices = [i for i, l in enumerate(line_list) if 'nop' in l or 'jmp' in l]
-    l = line_list[indices[i]]
-    if 'nop' in l:
-        l = l.replace('nop', 'jmp')
-    else:
-        l = l.replace('jmp', 'nop')
-    line_list[indices[i]] = l
-    return line_list
-
-
-funcs = {'nop': nop,
-         'jmp': jmp,
-         'acc': acc}
 
 lines = open('../inputs/day8.txt').readlines()
-
-part1, part2, replace_index, first_run = 0, 0, 0, True
-check, accumulator, idx, visited = lines.copy(), 0, 0, set()
-
-while idx != len(check):
-    while idx != len(check):
-        if idx in visited:
-            if first_run:
-                part1 = accumulator
-                first_run = False
-            break
-        visited.add(idx)
-        op, val = check[idx].strip().split()
-        f = funcs.get(op)
-        accumulator, idx = f(int(val), accumulator, idx)
-    if idx != len(check):
-        check, accumulator, idx, visited = lines.copy(), 0, 0, set()
-        check = replace_nop_jmp(check, replace_index)
-        replace_index += 1
-    else:
-        part2 = accumulator
+print(f"Part 1 Answer: {bootcode(lines)[1]}")
+for i, l in enumerate(lines):
+    if 'acc' in l:
+        continue
+    lines[i] = 'jmp' + l[3:] if 'nop' in l else 'nop' + l[3:]
+    success, val = bootcode(lines)
+    if success:
+        print(f"Part 2 Answer: {val}")
         break
-
-print(f"Part 1 Answer: {part1}")
-print(f"Part 2 Answer: {part2}")
+    else:
+        lines[i] = l
