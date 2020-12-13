a2c20f8ac494424f2aa50b7a6cc5f3315c2cbbcc
kevin <kevin.dunphy1989@gmail.com>
2020-12-12 23:34:51 -0400

    A little bit faster on the parsing - Set seats in a dictionary for faster indexing, skipping locations that have been handled, adding each pair of LOS/adjacencies at once rather than doing each one individually.
    Much quicker now, still kind of slow.

--- AOC2020/solutions/day11.py
+++ AOC2020/solutions/day11.py
@@ -1,10 +1,9 @@
 class Seat:
-    def __init__(self, location: complex):
-        self.location = location
+    def __init__(self):
         self.filled = False
         self.buffer = False
-        self.adjacent_seats = []
-        self.line_of_sight_seats = []
+        self.adjacent_seats = set()
+        self.line_of_sight_seats = set()
 
     def reset(self):
         self.filled = False
@@ -36,55 +35,61 @@ class Seat:
                 self.buffer = self.filled
 
     def commit(self):
-        self.filled = self.buffer
+        if self.filled != self.buffer:
+            self.filled = self.buffer
 
 
 def go(part2=False):
-    for seat in seats:
+    def update(part2=False):
+        for s in seats.values():
+            if part2:
+                s.update_p2()
+            else:
+                s.update_p1()
+        for s in seats.values():
+            s.commit()
+
+    for seat in seats.values():
         seat.reset()
 
-    prev_count = 0
-    for seat in seats:
-        if part2:
-            seat.update_p2()
-        else:
-            seat.update_p1()
-    for seat in seats:
-        seat.commit()
-    new_count = [seat.filled for seat in seats].count(True)
-
+    prev_count = [seat.filled for seat in seats.values()].count(True)
+    update(part2=part2)
+    new_count = [seat.filled for seat in seats.values()].count(True)
     while prev_count != new_count:
         prev_count = new_count
-        for seat in seats:
-            if part2:
-                seat.update_p2()
-            else:
-                seat.update_p1()
-        for seat in seats:
-            seat.commit()
-        new_count = [seat.filled for seat in seats].count(True)
-    return new_count
+        update(part2=part2)
+        new_count = [seat.filled for seat in seats.values()].count(True)
+    return prev_count
 
 
-seats = [Seat(complex(i, j)) for j, line in enumerate(open('../inputs/day11.txt')) for i, char in enumerate(line) if char == 'L']
-locations = [seat.location for seat in seats]
-max_real = max([int(location.real) for location in locations])
-max_imag = max([int(location.imag) for location in locations])
-for seat in seats:
-    location = seat.location
+seats = {}
+for j, line in enumerate(open('../inputs/day11.txt')):
+    for i, char in enumerate(line):
+        if char == 'L':
+            seats[complex(i, j)] = Seat()
+
+max_real = max([int(location.real) for location in seats.keys()])
+max_imag = max([int(location.imag) for location in seats.keys()])
+handled_locations = set()
+for location, seat in seats.items():
     for loc in [1j, 1+1j, 1, -1j, -1-1j, -1, 1-1j, -1+1j]:
         adj_loc = location + loc
+        if adj_loc in handled_locations:
+            continue
         if any([adj_loc.real < 0, adj_loc.real > max_real, adj_loc.imag < 0, adj_loc.imag > max_imag]):
             continue
-        if adj_loc in locations:
-            seat.adjacent_seats.append(seats[locations.index(adj_loc)])
+        if adj_loc in seats.keys():
+            seats[adj_loc].adjacent_seats.add(seat)
+            seat.adjacent_seats.add(seats[adj_loc])
         for i in range(1, max([max_real, max_imag])):
             los_loc = location + loc*i
             if any([los_loc.real < 0, los_loc.real > max_real, los_loc.imag < 0, los_loc.imag > max_imag]):
                 break
-            if los_loc in locations:
-                seat.line_of_sight_seats.append(seats[locations.index(los_loc)])
+            if los_loc in seats.keys():
+                seat.line_of_sight_seats.add(seats[los_loc])
+                seats[los_loc].line_of_sight_seats.add(seat)
                 break
+    handled_locations.add(location)
 
 print(f"Part 1 Answer: {go()}")
 print(f"Part 2 Answer: {go(part2=True)}")
