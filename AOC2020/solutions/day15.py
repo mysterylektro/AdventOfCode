3cd97d74d2c93dde9526dfb6eebbfb2d5edbef4b
kevin <kevin.dunphy1989@gmail.com>
2020-12-16 19:30:16 -0400

    Day 15 runs both part 1 and 2. Part 2 hella slow - is there some pattern we're supposed to find?

--- AOC2020/solutions/day15.py
+++ AOC2020/solutions/day15.py
@@ -1,22 +1,22 @@
-from collections import defaultdict
-spoken = defaultdict(list)
-last_turn = 0
-last_spoken = 0
-for i, j in enumerate(list(map(int, open('../inputs/day15.txt').readline().split(',')))):
-    last_spoken, last_turn = j, i+1
-    spoken[last_spoken].append(last_turn)
-
-for turn in range(last_turn+1, 30000001):
-    if len(spoken[last_spoken]) <= 1:
-        to_speak = 0
-    else:
-        to_speak = spoken[last_spoken][-1] - spoken[last_spoken][-2]
-    last_spoken = to_speak
-    spoken[last_spoken].append(turn)
-
-print(f"Turn: {turn}: {last_spoken}")
+from collections import defaultdict, deque
 
 
+def memory_game(end_num):
+    spoken = defaultdict(deque)
+
+    for i, j in enumerate(list(map(int, open('../inputs/day15.txt').readline().split(',')))):
+        spoken[j].append(i+1)
+
+    last_turn, last_spoken = i+1, j
+    for turn in range(last_turn+1, end_num+1):
+        last_spoken = 0 if len(spoken[last_spoken]) <= 1 else spoken[last_spoken][1] - spoken[last_spoken][0]
+        spoken[last_spoken].append(turn)
+        if len(spoken[last_spoken]) > 2:
+            spoken[last_spoken].popleft()
+
+    return last_spoken
 
 
+print(f"Part 1 Answer: {memory_game(2020)}")
+print(f"Part 2 Answer: {memory_game(30000000)}")
 
