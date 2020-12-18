e7a44807dfa3b3deedd2ce14c12f99e4cba30ee5
Kevin Dunphy <kevin.dunphy1989@gmail.com>
2020-12-18 13:19:49 -0400

    Day 18 solution

--- AOC2020/solutions/day18.py
+++ AOC2020/solutions/day18.py
@@ -1,19 +1,13 @@
 import operator
 import re
 
-operations = {'+': operator.add,
-              '*': operator.mul}
+operations = {'+': operator.add, '*': operator.mul}
 
 
 def innermost_parenthesis(expression):
-    s = [m.start() for m in re.finditer(r'\(', expression)]
-    e = [m.start() for m in re.finditer(r'\)', expression)]
-    if len(s) > 0:
-        e = e[0]
-        s = max([i for i in s if i < e])
-        return s, e
-    else:
-        return None, None
+    s, e = [m.start() for m in re.finditer(r'\(', expression)], [m.start() for m in re.finditer(r'\)', expression)]
+    s, e = (max([i for i in s if i < e[0]]), e[0]) if len(s) > 0 else (None, None)
+    return s, e
 
 
 def evaluate_p1(expression):
@@ -43,10 +37,8 @@ def evaluate_p2(expression):
     return eval(' '.join(tokens))
 
 
-p1, p2 = 0, 0
-for line in [line.strip() for line in open('../inputs/day18.txt')]:
-    p1 += evaluate_p1(line)
-    p2 += evaluate_p2(line)
+p1 = sum([evaluate_p1(line)for line in [line.strip() for line in open('../inputs/day18.txt')]])
+p2 = sum([evaluate_p2(line)for line in [line.strip() for line in open('../inputs/day18.txt')]])
 
 print(f"Part 1 Answer: {p1}")
 print(f"Part 2 Answer: {p2}")
