40a511a30a1f92602f3b29d69454e62628dd28bc
kevin <kevin.dunphy1989@gmail.com>
2020-12-21 11:46:08 -0400

    Day 21 solution cleaned up.

--- AOC2020/solutions/day21.py
+++ AOC2020/solutions/day21.py
@@ -1,6 +1,6 @@
 import re
 
-allergen_list = dict()
+allergen_dict = dict()
 food_set = set()
 food_list = []
 
@@ -11,20 +11,19 @@ for line in [line.strip() for line in open('../inputs/day21.txt')]:
     food_list.extend(foods)
     allergens = re.split(r', | |\)', allergens)[:-1]
     for allergen in allergens:
-        allergen_list[allergen] = set.intersection(allergen_list[allergen], set(foods)) if allergen in allergen_list else set(foods)
+        allergen_dict[allergen] = set.intersection(allergen_dict[allergen], set(foods)) if allergen in allergen_dict else set(foods)
 
-while sum([len(c) for c in allergen_list.values()]) != len(allergen_list):
-    # find sets that contain only a single value, and remove that value from other sets:
-    single_val_sets = [s for s in allergen_list.values() if len(s) == 1]
+while sum([len(c) for c in allergen_dict.values()]) != len(allergen_dict):
+    single_val_sets = [s for s in allergen_dict.values() if len(s) == 1]
     for s in single_val_sets:
         val = list(s)[0]
-        for allergen, candidates in allergen_list.items():
+        for candidates in allergen_dict.values():
             if val in candidates and len(candidates) != 1:
                 candidates.remove(val)
 
-safe_foods = set.union(*allergen_list.values()) ^ food_set
+safe_foods = set.union(*allergen_dict.values()) ^ food_set
 count = sum([food_list.count(food) for food in safe_foods])
 print(f'Part 1 Answer: {count}')
 
-allergen_foods = ','.join([list(allergen_list[key])[0] for key in sorted(allergen_list)])
+allergen_foods = ','.join([list(allergen_dict[key])[0] for key in sorted(allergen_dict)])
 print(f"Part 2 Answer: {allergen_foods}")
