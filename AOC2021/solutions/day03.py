e393ccf2d440c8e9869094b0db6f807cf457ae16
kevin <kevin.dunphy1989@gmail.com>
2021-12-03 13:13:04 -0400

    Day 3 solution - updated a bit cleaner.

--- AOC2021/solutions/day03.py
+++ AOC2021/solutions/day03.py
@@ -11,14 +11,12 @@ start = timeit.default_timer()
 # ---------- SOLUTION HERE ---------- #
 
 data = np.array([[int(i) for i in d] for d in puzzle.input_data.split('\n')])
-gamma_rate = [1 if np.sum(data[:, i]) >= data.shape[0] // 2 else 0 for i in range(data.shape[1])]
-gamma_rate_num = np.sum([2**i for i in range(data.shape[1]) if gamma_rate[::-1][i]])
-epsilon_rate_num = np.sum([2**i for i in range(data.shape[1]) if not gamma_rate[::-1][i]])
-
+gamma_rate = np.array([1 if np.sum(data[:, i]) >= data.shape[0] // 2 else 0 for i in range(data.shape[1])])
+gamma_rate_num = int(''.join(list(map(str, gamma_rate))), 2)
+epsilon_rate_num = int(''.join(list(map(str, np.abs(gamma_rate - 1)))), 2)
 P1_ANSWER = gamma_rate_num * epsilon_rate_num
 
 oxy_rating = co2_rating = data
-
 for i in range(data.shape[1]):
     if oxy_rating.shape[0] != 1:
         mask = oxy_rating[:, i] == 1
@@ -28,8 +26,9 @@ for i in range(data.shape[1]):
         mask = co2_rating[:, i] == 0
         co2_rating = co2_rating[mask] if co2_rating[mask].shape[0] <= co2_rating[~mask].shape[0] else co2_rating[~mask]
 
-oxy_rate_num = np.sum([2**i for i in range(12) if oxy_rating.flatten()[::-1][i]])
-co2_rate_num = np.sum([2**i for i in range(12) if co2_rating.flatten()[::-1][i]])
+oxy_rate_num = int(''.join(list(map(str, oxy_rating.flatten()))), 2)
+co2_rate_num = int(''.join(list(map(str, co2_rating.flatten()))), 2)
+
 
 P2_ANSWER = oxy_rate_num * co2_rate_num
 
