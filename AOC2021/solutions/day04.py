8b320ede832ba36d8f5906da930732d032465b3e
kevin <kevin.dunphy1989@gmail.com>
2021-12-04 10:45:11 -0400

    Better day 4 solution

--- AOC2021/solutions/day04.py
+++ AOC2021/solutions/day04.py
@@ -13,32 +13,24 @@ start = timeit.default_timer()
 data = puzzle.input_data.split('\n\n')
 numbers = np.fromiter(map(int, data[0].split(',')), dtype=int)
 boards = np.array([[list(map(int, row.split())) for row in board.split('\n')] for board in data[1:-1]])
-winners = np.full_like(boards, fill_value=False, dtype=bool)
+winners = np.full_like(boards, fill_value=0, dtype=int)
 
-winning_board = None
-P1_ANSWER = None
+P1_ANSWER = P2_ANSWER = None
 for num in numbers:
-    winners[boards == num] = True
-    # Check if winner exists
-    row_winners = np.array([[all(winners[i, j, :]) for j in range(winners.shape[1])] for i in range(winners.shape[0])])
-    col_winners = np.array([[all(winners[i, :, j]) for j in range(winners.shape[2])] for i in range(winners.shape[0])])
-    if any(row_winners.flatten()) or any(col_winners.flatten()):
-        # Find winning board
-        idx = np.argwhere(row_winners == True) if any(row_winners.flatten()) else np.argwhere(col_winners == True)
+    winners[boards == num] = 1
+    winning_boards = np.append(np.argwhere(np.sum(winners, axis=1) == boards.shape[1]),
+                               np.argwhere(np.sum(winners, axis=2) == boards.shape[2]), axis=0)
+    if len(winning_boards) > 0:
         if P1_ANSWER is None:
-            winning_board, marked = boards[idx[0, 0]], winners[idx[0, 0]]
-            unmarked_numbers = winning_board[~marked]
-            P1_ANSWER = np.sum(unmarked_numbers) * num
+            winning_board, marked = boards[winning_boards[0, 0]], winners[winning_boards[0, 0]]
+            P1_ANSWER = np.sum(winning_board[marked == 0]) * num
 
         if boards.shape[0] == 1:
-            marked = winners[0]
-            unmarked_numbers = boards[0][~marked]
-            P2_ANSWER = np.sum(unmarked_numbers) * num
+            P2_ANSWER = np.sum(boards[0][winners[0] == 0]) * num
             break
 
-        # Remove this board from consideration
-        boards = np.delete(boards, idx[:, 0], 0)
-        winners = np.delete(winners, idx[:, 0], 0)
+        boards = np.delete(boards, winning_boards[:, 0], 0)
+        winners = np.delete(winners, winning_boards[:, 0], 0)
 
 # ----------------------------------- #
 print(f"Finished in {timeit.default_timer() - start} seconds")
